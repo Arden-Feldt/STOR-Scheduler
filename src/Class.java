b0e93373491d1a592bc04a5bada711578ddd0862
Arden Feldt <feldt@ad.unc.edu>
2024-05-28 12:28:42 -0400

    start adding fields

---
+++ src/Class.java
@@ -0,0 +1,6 @@
+public class Class {
+    // There are 5 classes
+    // 2 big (100)
+    // 2 small (50)
+    // 1 with rolling chairs (40)
+}
--- src/Faculty/Faculty.java
+++ src/Faculty/Faculty.java
@@ -2,5 +2,9 @@ package Faculty;
 
 public class Faculty {
     private String name;
+    private int[] timeSlots;
+    boolean backToBack;
+    boolean getsOpinion;
+
     // Implementation
 }
