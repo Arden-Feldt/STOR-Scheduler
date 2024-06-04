8be167dfb5dadde3a1fdea123b5ac272fa47bf38
Arden Feldt <feldt@ad.unc.edu>
2024-06-04 12:32:50 -0400

    added roadmap

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        // TODO: Implement populate classes with the data
+
+        // TODO: implement Gurobi java API
+
+        // TODO: Run optimizer
+
+        // TODO: output optimal solution in legible format
+
     }
 }
