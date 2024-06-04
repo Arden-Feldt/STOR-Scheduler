c05f2e92bf37c6c4f7a541c245c492ed34793084
Arden Feldt <feldt@ad.unc.edu>
2024-06-04 11:51:15 -0400

    added highrequest to TimeSlot

--- src/Course/TimeSlot.java
+++ src/Course/TimeSlot.java
@@ -1,6 +1,17 @@
 package Course;
 
 public enum TimeSlot {
-    MWF800850,MWF905955,MWF10101100,MWF11151205,MWF1220110,MWF125215,MWF230320,MWF545635,TTH800915,TTH9301045highrequest,TTH11001215highrequest,TTH1230145highrequest,TTH200315,TTH330445,TTH500615,MW9051020,MW11151230,MW125240
+    MWF800850(),MWF905955,MWF10101100,MWF11151205,MWF1220110,MWF125215,MWF230320,MWF545635,
+    TTH800915,TTH9301045,TTH11001215(true),TTH1230145(true),TTH200315(true),TTH330445,TTH500615,MW9051020,
+    MW11151230,MW125240;
 
+    private final boolean highRequest;
+
+    TimeSlot(){this.highRequest = false;}
+
+    // TODO: Make sure i have the right highRequest times
+
+    TimeSlot(boolean highRequest) {
+        this.highRequest = highRequest;
+    }
 }
