e17e42913022a47c8f4f0fbcb0d129c81a4dda79
Arden Feldt <feldt@ad.unc.edu>
2024-07-01 18:50:48 -0400

    added a displayer that is currently empty

---
+++ src/ScheduleData/ScheduleDisplayer.java
@@ -0,0 +1,12 @@
+package ScheduleData;
+
+public class ScheduleDisplayer {
+    private String path;
+    public ScheduleDisplayer(String path){
+        this.path = path;
+    }
+
+    public void save_schedule(String path_again){
+
+    }
+}
