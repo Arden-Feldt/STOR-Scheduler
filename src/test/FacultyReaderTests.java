beb5c6812860f7aa66ebbb7de1cffc9066e97563
Arden Feldt <feldt@ad.unc.edu>
2024-07-02 14:54:14 -0400

    setting up space for tests

---
+++ src/test/FacultyReaderTests.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FacultyReaderTests {
+    @Test
+    public void unitTest1() {
+
+    }
+
+    @Test
+    public void equals_works() {
+        Object item = null;
+        assertFalse(item.equals(null));
+        assertTrue(item.equals(new Object()));
+    }
+}
+
