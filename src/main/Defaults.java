1dbf4e668f072ba5eb6fc579865b79e1b9231cc6
Arden Feldt <feldt@ad.unc.edu>
2024-12-08 15:24:21 -0500

    prep for 2025 run

--- src/main/Defaults.java
+++ src/main/Defaults.java
@@ -9,8 +9,7 @@ public class Defaults {
   // Paths
 
   public static final String PREFRENCEPATH = "src/main/Faculty/ProfessorData/ProPrefFalls23 - Copy.csv";
-  public static final String COURSEDATAPATH =
-      "src/main/Faculty/ProfessorData/TeachingAssignmentsWGrad.csv";
+  public static final String COURSEDATAPATH = "src/main/Faculty/ProfessorData/TeachingAssignmentsWGrad.csv";
   public static final String RAWSCHEDULEPATH = "src/main/Schedule/course_schedule.csv";
   public static final String DISPLAYSCHEDULECSVPATH = "src/main/Schedule/legible_schedule.csv";
 }
--- src/main/Schedule/CourseScheduler.java
+++ src/main/Schedule/CourseScheduler.java
@@ -61,6 +61,7 @@ public class CourseScheduler {
       constraints.gradStudentRoomConstraint(model, assign);
       constraints.enoughSeatsConstraint(model, assign);
       constraints.sixHundredOverlap(model, assign);
+      // TODO: Fix grad block
       //constraints.blockNextTimeSlotForGradCoursesAllDays(model, assign);
 
       // Optimize the model
